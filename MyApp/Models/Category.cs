1979a2f141ad6bd6e075f0920815af62c64cc340
sunnylalchandani <sunnylalchandani285@gmail,com>
2019-09-03 00:15:48 +0530

    commit first

---
+++ MyApp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApp.Models
+{
+    public class Category
+    {   [Required]
+        public int categoryId { get; set; }
+        [MaxLength(30)]
+        public string categoryName { get; set; }
+        public string description { get; set; }
+        public IEnumerable<drinks> drinks { get; set; }
+
+    }
+}
---
+++ MyApp/Models/drinks.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApp.Models
+{
+    public class drinks
+    {
+        [Required]
+        public int drinkId { get; set; }
+        [MaxLength(30)]
+        public string drinkName { get; set; }
+        [MaxLength(100)]
+        public string shortDescription { get; set; }
+        [MaxLength]
+        public string DetailDescription { get; set; }
+        [Required]
+        public int price { get; set; }
+        [Required]
+        public int storage { get; set; }
+        // public IFormFile Photo { get; set; }
+        [Required]
+        public string Photo { get; set; }
+        public string thumbnail { get; set; }
+
+        public int CategoryId { get; set; }
+        public bool inStock { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
