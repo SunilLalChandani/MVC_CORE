8472caeedf3cc911c0dc1c6fc698bb2aabbfb97d
sunnylalchandani <sunnylalchandani285@gmail,com>
2019-09-14 17:57:17 +0530

    CORONA BEER ADDED

--- MyApp/Models/Mock/MockDrinkRepo.cs
+++ MyApp/Models/Mock/MockDrinkRepo.cs
@@ -14,8 +14,9 @@ namespace MyApp.Models.Mock
                     new drinks { drinkId=1,drinkName="Bira",inStock=true,price=100,shortDescription="pint 330ml ,white bira",thumbnail="",CategoryId=2,Photo="https://cheers.com.np/uploads/products/978328434466665691222391259197415673358.png"},
 
                     new drinks { drinkId=2,drinkName="budwiser",inStock=true,price=120,shortDescription="pint 330ml ,budwiser magnum",thumbnail="",CategoryId=2,Photo="https://www.pngfind.com/pngs/m/230-2306616_budweiser-clipart-budweiser-beer-beer-can-and-bottle.png"}
+                    ,  new drinks { drinkId=3,drinkName="CORONA",inStock=true,price=120,shortDescription="pint 330ml ,CORONA LEMON",thumbnail="",CategoryId=2,Photo="https://www.pngfind.com/pngs/m/230-2306616_budweiser-clipart-budweiser-beer-beer-can-and-bottle.png"}
 
-                } ; } set => throw new NotImplementedException(); }
+                }; } set => throw new NotImplementedException(); }
         public IEnumerable<drinks> favDrink { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public drinks GetDrinksById(int id)
